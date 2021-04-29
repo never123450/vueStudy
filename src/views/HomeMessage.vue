a528648fc6bbc01e0dc42dae929e9eb04b2d37d7
徐文艳 <845619585@qq.com>
2021-04-29 14:38:54 +0800

    提交 HomeMessage.vue

---
+++ src/views/HomeMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li>消息 1</li>
+      <li>消息 2</li>
+      <li>消息 3</li>
+      <li>消息 4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeNews",
+};
+</script>
+
+<style scoped>
+
+</style>>
